282639cff9e01246b577bec1104de67afa5ec4c9
lim0n3 <simonestaffa96@gmail.com>
2025-03-17 16:19:26 +0000

    fix layout providers

--- app/app.tsx
+++ app/app.tsx
@@ -11,23 +11,6 @@ const Home = dynamic(() => import("@/components/Home"), {
   ),
 });
 
-const FrameProvider = dynamic(
-  () =>
-    import("@/components/farcaster-provider").then((mod) => mod.FrameProvider),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-purple-600" />
-      </div>
-    ),
-  }
-);
-
 export default function App() {
-  return (
-    <FrameProvider>
-      <Home />
-    </FrameProvider>
-  );
+  return <Home />;
 }
